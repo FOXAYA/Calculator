bae2370e308bda2a4325281656124eae4b03e2f5
FOXAYA <omniasaged111@gmail.com>
2024-11-23 16:58:35 +0200

    updated

--- main.js
+++ main.js
@@ -3,15 +3,24 @@ const input = document.querySelector("#display");
 function appendToDisplay(ele) {
   input.value += ele;
 }
-function calculate(){
-  try{
+function calculate() {
+  try {
     input.value = eval(input.value);
-  }
-  catch(error){
-    input.value = "Erorr"
+  } catch (error) {
+    input.value = "Erorr";
   }
 }
 
- function clearDisplay(){
-      input.value = "";
- }
+function clearDisplay() {
+  input.value = "";
+}
+function clearLastValue() {
+  input.value = input.value.slice(0, -1);
+}
+function sqrtValue() {
+  try {
+    input.value = Mathgit .sqrt(Number(input.value));
+  } catch (error) {
+    input.value = "Error";
+  }
+}
